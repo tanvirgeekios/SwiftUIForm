3ca11ab19e1c798f0ef01063f1326112cdb84290
tanvirgeekios <tanvir.alam@iosventure.com>
2021-01-15 09:10:20 +0600

    Initial Commit

---
+++ Form/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Form
+//
+//  Created by MD Tanvir Alam on 15/1/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
